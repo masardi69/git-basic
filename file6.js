187d8e345beb6078ea0a74cacdeaad3fa9e9937e
masardi69 <ardi.capolista@gmail.com>
2025-01-14 11:11:43 +0700

    perbaikan pada file6.js

--- file6.js
+++ file6.js
@@ -2,9 +2,9 @@ class Kelamin {
   constructor(nama, ukuran) {
     this.nama = nama;
     this.ukuran = ukuran;
-    this.kentu = (istri) => `nama saya ${this.nama}, saya memasukkan kontol saya yang berukuran ${this.ukuran} ke dalam vagina ${istri}`;
+    this.kentu = (istri) => `nama saya ${this.nama}, saya memasukkan kontol saya yang berukuran ${this.ukuran} cm ke dalam vagina ${istri}`;
   }
 }
 
-let ardi = new Kelamin("ardi", "22 cm");
-console.log(ardi.kentu("Leony"));
+let ardi = new Kelamin("ardi", 22);
+console.log(ardi.kentu("bernica Leony"));
